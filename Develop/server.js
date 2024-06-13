6abb71f4d043160bca4f8a3b86bb7adfeff38c07
Eric <ekellogg90@gmail.com>
2024-06-13 18:26:24 -0500

    made changes to file structure

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const api = require('./Develop/public/assets/routes/indexRoute');
+const api = require('./public/assets/routes/indexRoute');
 
 const PORT = process.env.port || 3001;
 const app = express();
@@ -11,16 +11,16 @@ app.use('/api', api);
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/Develop/public/index.html'))
-});
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/public/index.html'))
+// });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/Develop/public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/Develop/public/notes.html'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
 app.listen(PORT, () => {
